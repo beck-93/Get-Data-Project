286e611fbff7afc6d2f69870952b7df539379c2c
beck-93 <1rebeccabausinger@gmail.com>
2021-01-11 17:18:36 -0500

    unzip necessary files

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,7 @@
-This should go onto R
+#unzip files
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+if(!file.exists("./samsung")) {dir.create("./samsung")}
+download.file(url, destfile = "./samsung/samsung_data.zip", method = "curl")
+unzip(zipfile = "./samsung/samsung_data.zip", exdir = "C:/Users/1rebe/OneDrive/Documents/NSG586/rstudio/coursera/Get Data Project")
+head(unzip(zipfile = "./samsung/samsung_data.zip", list = TRUE)[["Name"]])
+path <- "samsung/UCI HAR Dataset/test/"
