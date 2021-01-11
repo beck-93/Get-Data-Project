b166e4e1191657aca7b1db0acad18e022d0981f2
beck-93 <1rebeccabausinger@gmail.com>
2021-01-11 17:51:27 -0500

    loading data into workspace

--- run_analysis.R
+++ run_analysis.R
@@ -4,4 +4,20 @@ if(!file.exists("./samsung")) {dir.create("./samsung")}
 download.file(url, destfile = "./samsung/samsung_data.zip", method = "curl")
 unzip(zipfile = "./samsung/samsung_data.zip", exdir = "C:/Users/1rebe/OneDrive/Documents/NSG586/rstudio/coursera/Get Data Project")
 head(unzip(zipfile = "./samsung/samsung_data.zip", list = TRUE)[["Name"]])
-path <- "samsung/UCI HAR Dataset/test/"
+#names in zip folder
+# "UCI HAR Dataset/activity_labels.txt"    "UCI HAR Dataset/features.txt"          
+# "UCI HAR Dataset/features_info.txt"      "UCI HAR Dataset/README.txt"            
+#"UCI HAR Dataset/test/"                  "UCI HAR Dataset/test/Inertial Signals/"
+
+#loading data into workspace
+install.packages("readtext")
+library(readtext)
+activity <- readtext("activity_labels.txt")
+testsubjects <- read.table("subject_test.txt")
+X_test <- read.table("X_test.txt")
+Y_test <- read.table("y_test.txt")
+trainsubjects <- read.table("subject_train.txt")
+X_train <- read.table("X_train.txt")
+Y_train <- read.table("y_train.txt")
+features <- read.table("features.txt")
+
